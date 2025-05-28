09e5f8a8db22d6e74004b7f4106a77a098d62822
Jimmy Ngai <jimmyngai@ssm.gov.mo>
2025-05-28 11:50:39 +0800

    docs: Update main message with Advent of Code 2024 and usage instructions

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Advent of Code 2024 - Rust Solutions");
+    println!("Use `cargo run --bin dayX` to run a specific day's solution");
+    println!("Replace X with the day number you want to run");
 }
